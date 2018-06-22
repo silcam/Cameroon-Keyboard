9d7e8f5ca5e84815e6b2991f71a777988921d205
Matthw Lee <matthew_lee@sil.org>
2018-06-22 01:13:43 -0400

    Updated CSS

---
+++ src/km/collectKBs.bat
@@ -0,0 +1,2 @@
+xcopy D:\Github\Cameroon-Keyboard\src\km\cameroon_azerty\source\build\cameroon_azerty.js D:\Github\CamKeyAndroid\app\src\main\assets\languages\cameroon_azerty.js /y
+xcopy D:\Github\Cameroon-Keyboard\src\km\cameroon_qwerty\source\build\cameroon_qwerty.js D:\Github\CamKeyAndroid\app\src\main\assets\languages\cameroon_qwerty.js /y
