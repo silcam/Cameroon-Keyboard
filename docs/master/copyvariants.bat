1f2d13ddfff06aeb4d28f11e334db5b2778ad941
Matthw Lee <matthew_lee@sil.org>
2018-10-24 17:10:55 -0400

    updated bat file

--- docs/master/copyvariants.bat
+++ docs/master/copyvariants.bat
@@ -1,2 +1,16 @@
+copy kb.css variants\kb.css
+copy welcome.htm variants\welcome.htm
+
+
+rmdir ..\..\src\km\sil_cameroon_azerty\source\welcome
+mkdir ..\..\src\km\sil_cameroon_azerty\source\welcome
 copy variants\welcome-az-km.htm ..\..\src\km\sil_cameroon_azerty\source\welcome\welcome.htm /y
+xcopy /s images ..\..\src\km\sil_cameroon_azerty\source\welcome
+copy kb.css ..\..\src\km\sil_cameroon_azerty\source\welcome\kb.css
+
+
+rmdir ..\..\src\km\sil_cameroon_qwerty\source\welcome
+mkdir ..\..\src\km\sil_cameroon_qwerty\source\welcome
 copy variants\welcome-qw-km.htm ..\..\src\km\sil_cameroon_qwerty\source\welcome\welcome.htm /y
+xcopy /s images ..\..\src\km\sil_cameroon_qwerty\source\welcome
+copy kb.css ..\..\src\km\sil_cameroon_qwerty\source\welcome\kb.css
